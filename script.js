3366c2645ebf59d472757120bed77b3ca959c5af
NICOLA-200 <fatepepe66@gmail.com>
2023-05-06 22:23:29 +0200

    added new things

---
+++ script.js
@@ -0,0 +1,7 @@
+const APP_ID = "5b4ca1ce8be44690a5accec5d646555d"
+
+const TOKEN = "007eJxTYNjLOrmQa+pDO8eDZZc1yzbtC/67fM0br99FuosMZZ+vKUpWYDBNMklONExOtUhKNTExszRINE1MTk5NNk0xMzEzNTVN2eQamtIQyMiw+NABJkYGCATx2RmSMxJLHAsKGBgAKhUigA=="
+
+const CHANNEL = "ChatApp"
+
+const client = AgoraRTC.createClient({mode:"rtc", codec: 'vp8'})
